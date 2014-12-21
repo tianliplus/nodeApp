a9f31e320914f56a442bb8f7a04f6aa6dc469666
Tian Li <tianliplus@gmail.com>
2014-12-20 22:47:42 -0500

    upgrade

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static(__dirname));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.use('/vote', routes.router);
 //app.use('/users', users);
